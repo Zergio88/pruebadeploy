fb38eec7764493e56219abdb7748c39b9183687f
Zer <sergiomamani@live.com.ar>
2022-12-01 17:19:17 -0300

    add login uri, horario controller, Direccion controller

--- src/main/java/com/pruebadeploy/pruebadeploy/recurso/getDistanceMatrixAPI.java
+++ src/main/java/com/pruebadeploy/pruebadeploy/recurso/getDistanceMatrixAPI.java
@@ -12,24 +12,32 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 
 public class getDistanceMatrixAPI {
 
-	private static final String API_KEY="AIzaSyAZXwHEMaYaB_NjWkS5qCwPHtIexc9IBv0";
-	public static float[][] distancia;
-	
-	public static String getData(String origen, String destino) throws IOException, InterruptedException {
-	
-		/*String url="https://maps.googleapis.com/maps/api/distancematrix/json?destinations=-34.6173505163973,-58.38178810357908&origins=-34.61679872494791,-58.52067584629976&key=AIzaSyAZXwHEMaYaB_NjWkS5qCwPHtIexc9IBv0"; */
-		String url="https://maps.googleapis.com/maps/api/distancematrix/json?destinations="+destino+"&origins="+origen+"&key="+API_KEY;
-		HttpRequest solicitud = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
-		HttpClient cliente = HttpClient.newBuilder().build();
-		String respuesta = cliente.send(solicitud, HttpResponse.BodyHandlers.ofString()).body();
-		
-		/* ObjectMapper mapper = new JsonMapper();
-		//JsonNode json = mapper.readTree(respuesta);
-		System.out.println(j));
-		
-		String distancia = json.get("rows").get("elements").get("distance").get("text").asText(); */
+	/*
+	 * private static final String
+	 * API_KEY="AIzaSyAZXwHEMaYaB_NjWkS5qCwPHtIexc9IBv0"; public static float[][]
+	 * distancia;
+	 * 
+	 * public static String getData(String origen, String destino) throws
+	 * IOException, InterruptedException {
+	 * 
+	 * String url=
+	 * "https://maps.googleapis.com/maps/api/distancematrix/json?destinations=-34.6173505163973,-58.38178810357908&origins=-34.61679872494791,-58.52067584629976&key=AIzaSyAZXwHEMaYaB_NjWkS5qCwPHtIexc9IBv0";
+	 * String
+	 * url="https://maps.googleapis.com/maps/api/distancematrix/json?destinations="+
+	 * destino+"&origins="+origen+"&key="+API_KEY; HttpRequest solicitud =
+	 * HttpRequest.newBuilder().GET().uri(URI.create(url)).build(); HttpClient
+	 * cliente = HttpClient.newBuilder().build(); String respuesta =
+	 * cliente.send(solicitud, HttpResponse.BodyHandlers.ofString()).body();
+	 */
+	/*
+	 * ObjectMapper mapper = new JsonMapper(); //JsonNode json =
+	 * mapper.readTree(respuesta); System.out.println(j));
+	 * 
+	 * String distancia =
+	 * json.get("rows").get("elements").get("distance").get("text").asText();
+	 */
 
-		return respuesta;
-	}
+	// return respuesta;
+	// }
 
 }
